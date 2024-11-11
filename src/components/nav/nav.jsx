09a5eede024e82424b6d90e6c49ac43e7ac3f0c5
imAbhaySingh <97874215+imAbhaySingh@users.noreply.github.com>
2024-11-11 13:00:19 +0530

    Update nav.jsx

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
+          <li><a href="#history">History</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
